952f0a7a430fe3a6975bb03f4f8465850c2d122c
scoutz <97310814+scoutzers85@users.noreply.github.com>
2022-11-12 19:51:11 +0000

    Update figurespawn.lua

--- figurespawn.lua
+++ figurespawn.lua
@@ -1,10 +1,16 @@
-Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
-local model = LoadCustomInstance("https://github.com/scoutzers85/what/blob/main/figure.rbxm?raw=true")
-model.Parent = workspace
-model:PivotTo(game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame)
-local animation = Instance.new("Animation")
-animation.AnimationId = "rbxassetid://8369184411"
-local testanim = model.Humanoid:LoadAnimation(animation)
-testanim:Play()
-testanim.Finished:Wait()
-model:Destroy()
+local figure = {}
+
+function figure.spawn()
+	Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
+	local model = LoadCustomInstance("https://github.com/scoutzers85/what/blob/main/figure.rbxm?raw=true")
+	model.Parent = workspace
+	model:PivotTo(game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame)
+	local animation = Instance.new("Animation")
+	animation.AnimationId = "rbxassetid://8369184411"
+	local testanim = model.Humanoid:LoadAnimation(animation)
+	testanim:Play()
+	wait(3)
+	model:Destroy()
+end
+
+return figure
