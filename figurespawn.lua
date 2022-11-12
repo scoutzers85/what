74f7c860e5a5ea8a250a70dc63520614a2073469
scoutz <97310814+scoutzers85@users.noreply.github.com>
2022-11-12 20:07:25 +0000

    Update figurespawn.lua

--- figurespawn.lua
+++ figurespawn.lua
@@ -1,14 +1,18 @@
 local figure = {}
 
+function figure.doanimation(anim, figure)
+	local animation = Instance.new("Animation")
+	animation.AnimationId = anim
+	local animationl = model.Humanoid:LoadAnimation(animation)
+	animationl:Play()
+end
+
 function figure.spawn()
 	Functions = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Functions.lua"))()
 	local model = LoadCustomInstance("https://github.com/scoutzers85/what/blob/main/figure.rbxm?raw=true")
 	model.Parent = workspace
 	model:PivotTo(game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame)
-	local animation = Instance.new("Animation")
-	animation.AnimationId = "rbxassetid://8369184411"
-	local testanim = model.Humanoid:LoadAnimation(animation)
-	testanim:Play()
+	figure.doanimation("rbxassetid://8369184411", model)
 	wait(3)
 	model:Destroy()
 end
